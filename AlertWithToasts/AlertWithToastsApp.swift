54ee44f49ebe0566cccaf69871aa1ef5c5486e58
adamcaak <ladamczak1997@gmail.com>
2025-08-10 22:31:31 +0200

    Initial Commit

---
+++ AlertWithToasts/AlertWithToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertWithToastsApp.swift
+//  AlertWithToasts
+//
+//  Created by Łukasz Adamczak on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlertWithToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
